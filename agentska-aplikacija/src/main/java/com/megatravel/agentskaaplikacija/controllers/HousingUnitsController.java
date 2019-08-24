03a6b340341a0816552aca92118891b466529e50
Djuza <nikola.vojvodic0@gmail.com>
2019-08-24 17:25:52 +0200

    Podesena klasa

--- agentska-aplikacija/src/main/java/com/megatravel/agentskaaplikacija/controllers/HousingUnitsController.java
+++ agentska-aplikacija/src/main/java/com/megatravel/agentskaaplikacija/controllers/HousingUnitsController.java
@@ -1,5 +1,7 @@
 package com.megatravel.agentskaaplikacija.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,12 +16,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.megatravel.agentskaaplikacija.dtos.HousingUnitDTO;
 import com.megatravel.agentskaaplikacija.model.HousingUnit;
 import com.megatravel.agentskaaplikacija.services.HousingUnitsService;
-
-
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -31,16 +32,27 @@ public class HousingUnitsController {
 	@RequestMapping(method = RequestMethod.GET,
 			value = "/rest/units/search",
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<HousingUnitDTO>> search(@RequestParam("country") String country,
+	public ResponseEntity<List<HousingUnitDTO>> search(@RequestParam(name = "country", required = false) String country,
 			@RequestParam(name = "city", required = false) String city,
-			@RequestParam(name = "people", required = false) int people,
-			@RequestParam(name = "start", required = false) Date start,
-			@RequestParam(name = "end", required = false) Date end) {
-		List<HousingUnitDTO> results = new ArrayList<HousingUnitDTO>();
-		for(HousingUnit unit : this.housingUnitsService.search(country, city, people, start, end)) {
-			results.add(new HousingUnitDTO(unit));
+			@RequestParam(name = "people", required = false) Integer people,
+			@RequestParam(name = "start", required = false) String startString,
+			@RequestParam(name = "end", required = false) String endString) {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			Date start = null;
+			Date end = null;
+			if(startString != null && endString != null) {
+				start = format.parse(startString);
+				end = format.parse(endString);
+			}
+			List<HousingUnitDTO> results = new ArrayList<HousingUnitDTO>();
+			for(HousingUnit unit : this.housingUnitsService.search(country, city, people, start, end)) {
+				results.add(new HousingUnitDTO(unit));
+			}
+			return new ResponseEntity<List<HousingUnitDTO>>(results, HttpStatus.OK);
+		} catch (ParseException e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
 		}
-		return new ResponseEntity<List<HousingUnitDTO>>(results, HttpStatus.OK);
 	}
 	
 	@RequestMapping(method = RequestMethod.POST,
