1896d24080623bfa6069654d3134b1f2dcdf9b29
Vlado <vladimir.jovanovic.apatin@gmail.com>
2019-09-05 21:19:04 +0200

    zuul gateway

---
+++ zuul-gateway-load-balancer/src/main/java/com/megatravel/zuulgatewayloadbalancer/Zuul.java
@@ -0,0 +1,17 @@
+package com.megatravel.zuulgatewayloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class Zuul {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zuul.class, args);
+	}
+
+}
